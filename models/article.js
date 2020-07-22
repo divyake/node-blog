eab3eb3e041a4ff09ffeb8f7a7d4b2fdbe333c34
Divya Prakash <divya18.prak@gmail.com>
2020-07-22 15:56:00 +0530

    update article.js

--- models/article.js
+++ models/article.js
@@ -8,7 +8,7 @@ const dompurify = createDomPurify(new JSDOM().window)
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   description: {
     type: String
