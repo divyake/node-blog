17d356cdbb781190e73282b97b03a433d321786c
Divya Prakash <divya18.prak@gmail.com>
2020-07-22 15:53:41 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
-const app = express();
-
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true,useUnifiedTopology: true  } , (req,res) => {
-    console.log('connected to db');
-});
-
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
-app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
 app.use('/articles', articleRouter)
 
-app.get('/', (req,res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Article description'
-    },
-
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Article 2 description'
-    }
-
-];
-    res.render('articles/index' , {articles : articles})
-})
-
-app.listen(5000);
+app.listen(5000)
